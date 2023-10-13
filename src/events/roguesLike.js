37160eeb3cb1ad2a87bad0c3715a85c2935cc629
achepta <yhEhocbk3RFy6dytT8Zv>
2023-10-14 02:03:39 +0300

    rogues like update

--- src/events/roguesLike.js
+++ src/events/roguesLike.js
@@ -1,6 +1,9 @@
+import {setTimer} from "../utils/eventUtils";
+
 const {setLeaderboard} = require("../leaderboard");
 export default function roguesLikeEvent() {
     if (/rogue_like_event\.php$/.test(location.href)) {
-        setLeaderboard(Array.from(document.querySelectorAll(".global_container_block")).at(-1).children[1], "beforeend", true)
+        setTimer(document.querySelector(".global_container_block_header"))
+        setLeaderboard(Array.from(Array.from(document.querySelectorAll(".global_container_block")).at(-1).getElementsByTagName("center")).at(-1))
     }
 }
